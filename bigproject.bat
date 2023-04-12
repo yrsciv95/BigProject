639e24dda879069335a89238894a9bec21fa9ce0
Amani Yoris <127019608+yrsciv95@users.noreply.github.com>
2023-04-12 22:15:09 +0200

    Add files via upload

---
+++ bigproject.bat
@@ -0,0 +1,13 @@
+cls
+@echo off
+echo.
+echo BigProject Launcher   -   Microsoft Corp.
+echo.
+echo *****************************************
+echo.
+pause
+echo.
+echo.
+@echo off
+cd C:\Users\yorisamani\Documents\perso\nsi perso\python\BigProject
+python bigproject.py
